d556f7e90ef21df8986f5a0d90b8c601d88dbda4
Kristof Kovacs <kristof.kovacs1996@gmail.com>
2024-05-19 13:37:35 +0200

    FIX:: change resolution of the depreciation calculation to 10 000 km

--- olympus_calculator/car.py
+++ olympus_calculator/car.py
@@ -2,12 +2,12 @@ def calculate_car_depreciation(
     initial_price: float, driven_years: int, driven_kms: int
 ) -> float:
     YEAR_DEPRECIATION = 0.1  # every year
-    KM_DEPRECIATION = 0.1  # every 50000 km
+    KM_DEPRECIATION = 0.021  # every 10000 km
 
     year_depreciation_factor = (1 - YEAR_DEPRECIATION) ** driven_years
     depreciation_from_years = initial_price * (1 - year_depreciation_factor)
 
-    km_depreciation_factor = (1 - KM_DEPRECIATION) ** (driven_kms // 50000)
+    km_depreciation_factor = (1 - KM_DEPRECIATION) ** (driven_kms // 10000)
     depreciation_from_kms = initial_price * (1 - km_depreciation_factor)
 
     total_depreciation = depreciation_from_years + depreciation_from_kms
