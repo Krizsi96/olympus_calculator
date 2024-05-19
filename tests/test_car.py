2960f032f4a861a58dbc3d64bf3042ec99229857
Kristof Kovacs <kristof.kovacs1996@gmail.com>
2024-05-19 12:12:42 +0200

    TEST:: add test case for depreciation calculation by mileage

--- tests/test_car.py
+++ tests/test_car.py
@@ -17,7 +17,20 @@ def test_car_depreciation_0_years_0_mileage():
 def test_car_depreciation_for_age(car_price, driven_years, expected_value):
     driven_km = 0
     depriciated_value = calculate_car_depreciation(car_price, driven_years, driven_km)
-    assert depriciated_value == expected_value
+    assert depriciated_value == pytest.approx(expected_value, rel=1e-2)
+
+
+@pytest.mark.parametrize(
+    "car_price, driven_km, expected_value",
+    [(100, 50000, 85), (100, 100000, 72.25), (100, 150000, 61.41)],
+)
+def test_car_depreciation_for_mileage(car_price, driven_km, expected_value):
+    """
+    the car's value should be reduced by 15% for each 50000 km driven
+    """
+    driven_years = 0
+    depriciated_value = calculate_car_depreciation(car_price, driven_years, driven_km)
+    assert depriciated_value == pytest.approx(expected_value, rel=1e-2)
 
 
 # the function should ask for the car's price, the number of years of the usage and for the total number of kilometers
