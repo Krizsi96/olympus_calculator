156fe2c8bd4495fb40895e5461579e6f255e113d
Kristof Kovacs <kristof.kovacs1996@gmail.com>
2024-05-19 12:18:23 +0200

    REFACTOR:: test cases

--- tests/test_car.py
+++ tests/test_car.py
@@ -3,38 +3,57 @@ from olympus_calculator.car import calculate_car_depreciation
 
 
 def test_car_depreciation_0_years_0_mileage():
+    # Given
     car_price = 100
     driven_years = 0
     driven_km = 0
+
+    # When
     depriciated_value = calculate_car_depreciation(car_price, driven_years, driven_km)
+
+    # Then
     assert depriciated_value == 100
 
 
 @pytest.mark.parametrize(
-    "car_price, driven_years, expected_value",
+    "car_price_parameter, driven_years_parameter, expected_value",
     [(100, 1, 90), (100, 2, 81), (100, 3, 72.9)],
 )
-def test_car_depreciation_for_age(car_price, driven_years, expected_value):
+def test_car_depreciation_for_age(
+    car_price_parameter, driven_years_parameter, expected_value
+):
+    """
+    the car's value should be reduced by 10% each year
+    """
+    # Given
     driven_km = 0
+    car_price = car_price_parameter
+    driven_years = driven_years_parameter
+
+    # When
     depriciated_value = calculate_car_depreciation(car_price, driven_years, driven_km)
+
+    # Then
     assert depriciated_value == pytest.approx(expected_value, rel=1e-2)
 
 
 @pytest.mark.parametrize(
-    "car_price, driven_km, expected_value",
+    "car_price_parameter, driven_km_parameter, expected_value",
     [(100, 50000, 85), (100, 100000, 72.25), (100, 150000, 61.41)],
 )
-def test_car_depreciation_for_mileage(car_price, driven_km, expected_value):
+def test_car_depreciation_for_mileage(
+    car_price_parameter, driven_km_parameter, expected_value
+):
     """
     the car's value should be reduced by 15% for each 50000 km driven
     """
+    # Given
     driven_years = 0
+    car_price = car_price_parameter
+    driven_km = driven_km_parameter
+
+    # When
     depriciated_value = calculate_car_depreciation(car_price, driven_years, driven_km)
+
+    # Then
     assert depriciated_value == pytest.approx(expected_value, rel=1e-2)
-
-
-# the function should ask for the car's price, the number of years of the usage and for the total number of kilometers
-# the function should return the car's depreciated value
-# when the year is 0, the car's value should be the same as the price
-# the car value should be reduced by 10% each year
-# the car's value should be reduced by 15% for each 50000 km
