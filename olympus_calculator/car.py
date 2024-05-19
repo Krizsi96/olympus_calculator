4c2911fcc6872d3769621bc6d4ea849e20274e78
Kristof Kovacs <kristof.kovacs1996@gmail.com>
2024-05-19 12:13:10 +0200

    FEAT:: calculate depreciated value from mileage

--- olympus_calculator/car.py
+++ olympus_calculator/car.py
@@ -1,4 +1,9 @@
 def calculate_car_depreciation(
     initial_price: float, driven_years: int, driven_kms: int
 ) -> float:
-    return initial_price * (0.9**driven_years)
+    depreciation_from_years = initial_price - initial_price * (0.9**driven_years)
+    depreciation_from_kms = initial_price - initial_price * (
+        0.85 ** (driven_kms // 50000)
+    )
+    total_depreciation = depreciation_from_years + depreciation_from_kms
+    return initial_price - total_depreciation
