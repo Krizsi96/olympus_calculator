ef670ad702797b3447676ec5103fdb1a81790c15
Kristof Kovacs <kristof.kovacs1996@gmail.com>
2024-05-19 12:45:36 +0200

    REFACTOR:: separate calculation for readability

--- olympus_calculator/car.py
+++ olympus_calculator/car.py
@@ -1,7 +1,14 @@
 def calculate_car_depreciation(
     initial_price: float, driven_years: int, driven_kms: int
 ) -> float:
-    depreciation_from_years = initial_price * (1 - (0.9**driven_years))
-    depreciation_from_kms = initial_price * (1 - (0.9 ** (driven_kms // 50000)))
+    YEAR_DEPRECIATION = 0.1  # every year
+    KM_DEPRECIATION = 0.1  # every 50000 km
+
+    year_depreciation_factor = (1 - YEAR_DEPRECIATION) ** driven_years
+    depreciation_from_years = initial_price * (1 - year_depreciation_factor)
+
+    km_depreciation_factor = (1 - KM_DEPRECIATION) ** (driven_kms // 50000)
+    depreciation_from_kms = initial_price * (1 - km_depreciation_factor)
+
     total_depreciation = depreciation_from_years + depreciation_from_kms
     return initial_price - total_depreciation
